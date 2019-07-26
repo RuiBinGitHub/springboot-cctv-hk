e41cab2abd31f51f006f7b625572ca8db289a8d8
RuiBin <2770471187@qq.com>
2019-07-26 10:54:21 +0800

    时间

--- src/main/java/com/springboot/MainApplication.java
+++ src/main/java/com/springboot/MainApplication.java
@@ -2,10 +2,8 @@ package com.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(value = "com.springboot")
 public class MainApplication {
 
 	public static void main(String[] args) {
