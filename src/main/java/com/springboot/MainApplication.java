ff19e794c739a339ac9aa04009e9edaa62ad2394
RuiBin <2770471187@qq.com>
2019-07-29 17:37:37 +0800

    2019-7-29 17:37:35

--- src/main/java/com/springboot/MainApplication.java
+++ src/main/java/com/springboot/MainApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MainApplication {
 
+	/** 项目主程序 */
 	public static void main(String[] args) {
 		SpringApplication.run(MainApplication.class, args);
 	}
